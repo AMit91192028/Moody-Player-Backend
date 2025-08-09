ad84ff0e9bc0e475d743e21cfebb0506dc462c42
AMit91192028 <amit3192352@gmail.com>
2025-08-09 21:46:33 +0530

    cors spelling change

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express  = require('express');
 const SongsRoutes = require('./routes/songs.routes');  
 const song = require('./model/songs.model');
-const cros = require('cors');
+const cors = require('cors');
 
 const app= express()
 
