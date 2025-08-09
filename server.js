3652a59b0d210182b217925b0b3e9e635714eb17
AMit91192028 <amit3192352@gmail.com>
2025-08-09 21:36:25 +0530

    removing dotenv

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-require('dotenv').config();
 const app = require('./src/app.js')
 const connectDB = require('./src/db/db.js')
 connectDB()
 
 app.listen(3000,()=>{
-    console.log("server is connect to 3000")
+    console.log(`server is connect to 3000`)
 })
