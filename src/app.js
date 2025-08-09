a1250ac4fa92bad47c8f04c527033e7d818fd4f8
AMit91192028 <amit3192352@gmail.com>
2025-08-09 22:17:21 +0530

    orgin upadted

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app= express()
 
 app.use(express.json());
-const allowedOrigins = ['https://moody-player-fronted-arbd.vercel.app', 'http://localhost:3000'];
+const allowedOrigins = ['https://moody-player-fronted.vercel.app/', 'http://localhost:3000'];
 
 app.use(cors({
   origin: function(origin, callback) {
